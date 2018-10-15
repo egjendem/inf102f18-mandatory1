3fe835bec518b88811f661f06596698c9e1d809a
Espen Gjendem <espen.gjendem@gmail.com>
2018-10-15 11:48:01 +0200

    changed add method

--- src/main/java/no/uib/ii/inf102/f18/mandatory1/IndexMinPQ.java
+++ src/main/java/no/uib/ii/inf102/f18/mandatory1/IndexMinPQ.java
@@ -11,49 +11,55 @@ public class IndexMinPQ<Key extends Comparable<Key>> implements IIndexPQ<Key> {
     Key[] heap;
 
     IndexMinPQ(int length) {
-        heap = new Key[length];
+        heap = (Key[]) new Comparable[length];
     }
 
     public static void main(String[] args) {
         IndexMinPQ<String> obj = new IndexMinPQ<>(10);
 
-        obj.add("amalie");
+        obj.add(1,"amalie");
+        // .. 9
+        obj.add(10,"espen");
     }
 
-    void add(int index, Key key) {
+    public void add(int index, Key key) {
+
         heap[index] = key;
     }
 
-    void changeKey(int index, Key key) {
+    public void changeKey(int index, Key key) {
         //TODO
     }
 
-    boolean contains(int index) {
-        //TODO
+    public boolean contains(int index) {
         return true;
     }
 
-    void delete(int index){
+    public void delete(int index){
 
     }
 
-    Key getKey(int index) {
-
+    public Key getKey(int index) {
+        return null;
     }
 
-    Key peekKey() {
-
+    public Key peekKey() {
+        return null;
     }
 
-    int peek() {
-
+    public int peek() {
+        return 0;
     }
 
-    int poll() {
-
+    public int poll() {
+        return 0;
     }
 
-    boolean isEmpty(){
-    return true;
+    public boolean isEmpty(){
+        return true;
+    }
+
+    public int size() {
+        return 0;
     }
 }
