9bcfce92ec0895138b733540e4a38ebb13ad3c5a
Amalie Helleberg Rovik <Amalie.Rovik@student.uib.no>
2018-10-15 15:44:54 +0200

    Lagd masse metoder i PQ

--- src/main/java/no/uib/ii/inf102/f18/mandatory1/IndexMinPQ.java
+++ src/main/java/no/uib/ii/inf102/f18/mandatory1/IndexMinPQ.java
@@ -8,9 +8,13 @@ package no.uib.ii.inf102.f18.mandatory1;
  */
 public class IndexMinPQ<Key extends Comparable<Key>> implements IIndexPQ<Key> {
     // Heap
-    Key[] heap;
+    private Key[] heap;
 
-    IndexMinPQ(int length) {
+    private int n;
+    private int[] pq;
+    private int[] qp;
+
+    public IndexMinPQ(int length) {
         heap = (Key[]) new Comparable[length];
     }
 
@@ -28,27 +32,42 @@ public class IndexMinPQ<Key extends Comparable<Key>> implements IIndexPQ<Key> {
     }
 
     public void changeKey(int index, Key key) {
-        //TODO
+        if (i < 0 || i >= maxN) throw new IllegalArgumentException();
+        if (!contains(i)) throw new NoSuchElementException("index is not in the priority queue");
+        keys[i] = key;
+        swim(qp[i]);
+        sink(qp[i]);
     }
 
     public boolean contains(int index) {
-        return true;
+        return qp[k] != -1;
     }
 
-    public void delete(int index){
-
+    public void delete(int index) {
+        if (i < 0 || i >= maxN) throw new IllegalArgumentException();
+        if (!contains(i)) throw new NoSuchElementException("index is not in the priority queue");
+        int index = qp[i];
+        exchange(index, n--);
+        swim(index);
+        sink(index);
+        keys[i] = null;
+        qp[i] = -1;
     }
 
     public Key getKey(int index) {
-        return null;
+        if( index < 0 || index >=maxN) throw new IllegalArgumentException();
+        // TODO Auto-generated method stub
+        if (!this.contains(index)) {
+            throw new IllegalArgumentException("Index not in priority queue");
+        }else return keys[index];
     }
 
     public Key peekKey() {
-        return null;
+        return keys[pq[1]];
     }
 
     public int peek() {
-        return 0;
+        return pq[1];
     }
 
     public int poll() {
@@ -56,10 +75,43 @@ public class IndexMinPQ<Key extends Comparable<Key>> implements IIndexPQ<Key> {
     }
 
     public boolean isEmpty(){
-        return true;
+        return n == 0;
     }
 
     public int size() {
-        return 0;
+        return n;
+    }
+
+    public Item min() {
+        return keys[pq[1]];
+    }
+
+    private void exchange(int i, int j) {
+        int swap = pq[i];
+        pq[i] = pq[j];
+        pq[j] = swap;
+        qp[pq[i]] = i;
+        qp[pq[j]] = j;
+    }
+
+    private void swim(int k) {
+        while (k > 1 && greater(k/2, k)) {
+            exchange(k, k/2);
+            k = k/2;
+        }
+    }
+
+    private void sink(int k) {
+        while (2*k <= n) {
+            int j = 2*k;
+            if (j < n && greater(j, j+1)) j++;
+            if (!greater(k, j)) break;
+            exchange(k, j);
+            k = j;
+        }
+    }
+
+    private boolean greater(int i, int j) {
+        return keys[pq[i]].compareTo(keys[pq[j]]) > 0;
     }
 }
